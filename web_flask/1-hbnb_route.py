53e7e55b92e0afb419fc789dab2d4b6ac672c88b
fafi-make <tebibu374@gmail.com>
2024-01-02 07:11:45 +0000

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,44 +1,23 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""the `1-hbnb_route` module
-starts a flask web application listening on `0.0.0.0:5000`
-"""
-
-from flask import Flask
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route("/", strict_slashes=False)
-def index():
-    """returns `Hello HBNB!` message"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """returns `HBNB` message"""
-    return "HBNB"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-=======
-"""
-starts a Flask web application
-"""
+""" script that starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes: /: display “Hello HBNB!”
+            /hbnb: display “HBNB”
+    You must use the option strict_slashes=False in your route definition """
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello_hbnb():
+    """ hello_hbnb method """
+    return ('Hello HBNB!')
+
+
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+def only_hbnb():
+    """ only_hbnb method """
+    return ('HBNB')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
->>>>>>> 212529db4b955a72d996660137790c2497399ff5
+    app.run(host='0.0.0.0', port=5000)
